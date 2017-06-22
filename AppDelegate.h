197cf15a7cfffd871b0ff2974e332f9cf533d583
fengziking <794505782@qq.com>
2017-06-22 16:08:39 +0800

    as
    开屏页-视频

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TailAndVideo
+//
+//  Created by 刘建明 on 17/6/6.
+//  Copyright © 2017年 刘建明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
